ab0a319670e0c3d12be881542272da00a2928a76
SolaRita <ritasolacanals@gmail.com>
2022-03-13 19:06:27 +0100

    black background

--- src/components/cardPlace/CardPlace.jsx
+++ src/components/cardPlace/CardPlace.jsx
@@ -1,21 +1,21 @@
 
 import styles from "../../styles/modulos/CardPlace.module.css";
 
-export default function CardPlace(props) {
+export default function CardPlace(place) {
 
  
   return (
     <div className={styles.card}>
       <div className={styles.left}>
         <div className={styles.text}>
-          <h4>{props.name}</h4>
-          <h5>{props.shortDescription}</h5>
+          <h4>{place.name}</h4>
+          <h5>{place.shortDescription}</h5>
         </div>
       </div>
       <div className={styles.rigth}>
         <div className={styles.text}>
-          <h6>{props.district}</h6>
-          <h6>{props.price}</h6>
+          <h6>{place.district}</h6>
+          <h6>{place.price}</h6>
         </div>
       </div>
     </div>

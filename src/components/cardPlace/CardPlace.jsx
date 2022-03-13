e6fd4d0b78ccf26a333b0966eff3de0119a5809f
SolaRita <ritasolacanals@gmail.com>
2022-03-13 18:46:59 +0100

    conect back and front

--- src/components/cardPlace/CardPlace.jsx
+++ src/components/cardPlace/CardPlace.jsx
@@ -1,18 +1,21 @@
+
 import styles from "../../styles/modulos/CardPlace.module.css";
 
-export default function CardPlace() {
+export default function CardPlace(props) {
+
+ 
   return (
     <div className={styles.card}>
       <div className={styles.left}>
         <div className={styles.text}>
-          <h4>Name</h4>
-          <h5>Short Description</h5>
+          <h4>{props.name}</h4>
+          <h5>{props.shortDescription}</h5>
         </div>
       </div>
       <div className={styles.rigth}>
         <div className={styles.text}>
-          <h6>District</h6>
-          <h6>Price</h6>
+          <h6>{props.district}</h6>
+          <h6>{props.price}</h6>
         </div>
       </div>
     </div>
--- src/components/listPlaces/ListPlaces.jsx
+++ src/components/listPlaces/ListPlaces.jsx
@@ -1,15 +1,34 @@
 import styles from "../../styles/modulos/ListPlace.module.css"
 import CardPlace from "../cardPlace/CardPlace"
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function ListPlaces () {
+
+
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/places").then((res) => {
+      setPlaces(res.data);
+    });
+  });
+
+
     return (
-        <div className={styles.frame_list}>
+        <div className={styles.frameList}>
             <h2>Category</h2>
-            <CardPlace/>
-            <CardPlace/>
-            <CardPlace/>
-            <CardPlace/>
-            <CardPlace/>
+
+          {places.map(({name, shortDescription, district, price })=>(
+            <CardPlace
+            name={name}
+            shortDescription={shortDescription}
+            district = {district}
+            price = {price}/>
+          ))}
+
+     
+
         </div>
     )
 }
